d8827089f68b80afc7b8230b604fe17f0aa16d43
sahil-github001 <sahil.official.001@outlook.com>
2023-06-15 19:58:12 +0530

    navbar and footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,8 +21,9 @@ const Hero = () => {
         <div className="hero__image-container">
             <div className="hero__image">
                 <Image src="/hero.png" alt="icon" fill className="object-contain"/>
-                <div className="hero__image-overlay"/>
+
             </div>
+            <div className="hero__image-overlay"/>
         </div>
     </div>
   )
